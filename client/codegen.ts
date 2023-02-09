53d9f9bddfeac18c1c12b65b55605910041528fe
Eleni Papanicolas <elenipapanicolas@gmail.com>
2023-02-09 17:12:57 -0500

    config( ): fix schema location for graphql codegen

--- client/codegen.ts
+++ client/codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: 'http://localhost:8081/graphql',
+  schema: 'https://one-day-app.onrender.com/graphql',
   documents: ['src/**/*.tsx'],
   generates: {
     './src/__generated__/': {
